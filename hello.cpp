392d36a802633d21dcadae48a1c8c2a73c16703d
Tomasz Sterna <tomek@xiaoka.com>
2015-10-01 23:06:54 +0200

    Basic KernelKit to-be skeleton

---
+++ hello.cpp
@@ -0,0 +1,49 @@
+#include <Application.h>  
+#include <Window.h>  
+#include <StringView.h>  
+  
+class HelloWorldView : public BStringView {  
+public:  
+    HelloWorldView(BRect rect, const char *name, const char*text):  
+        BStringView(rect, name, text) {  
+            SetFont(be_bold_font);
+            SetFontSize(24);
+    }  
+};
+  
+class HelloWorldWin : public BWindow {  
+public:   
+    HelloWorldWin(BRect frame) :  
+            BWindow(frame, "Hello", B_TITLED_WINDOW,  
+                B_NOT_RESIZABLE | B_NOT_ZOOMABLE) {  
+        HelloWorldView *view;
+        BRect rect(Bounds());
+        view = new HelloWorldView(rect, "HelloWorldView", "Hello, World!");
+        AddChild(view);
+    }     
+      
+    virtual bool QuitRequested() {  
+        be_app->PostMessage(B_QUIT_REQUESTED);     
+        return true;
+    }  
+};
+  
+class HelloWorldApp: public BApplication {  
+public:  
+    HelloWorldApp() :  
+        BApplication("application/x-vnd.Be-HelloWorld") {  
+    HelloWorldWin *wnd;
+    BRect rect;
+  
+    rect.Set(100, 80, 260, 120);
+    wnd = new HelloWorldWin(rect);
+  
+    wnd->Show();
+    }         
+};
+  
+int main(int argc, char **argv) {  
+    HelloWorldApp app;
+    app.Run();
+    return 0;
+}
