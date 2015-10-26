19e6887cf3d528afaef7d141d55d791922ffe552
Tomasz Sterna <tomek@xiaoka.com>
2015-10-27 00:14:38 +0100

    Implemented proper thread suspend/resume

--- thread.cpp
+++ thread.cpp
@@ -1,5 +1,8 @@
 #include <KernelKit.h>
 #include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/syscall.h>
 
 int32 receiver(void *data)
 {
@@ -7,10 +10,19 @@ int32 receiver(void *data)
    int32 code;
    char buf[512];
 
+   printf("[thread] in thread %d:%d\n", getpid(), syscall(SYS_gettid));
    code = receive_data(&sender, (void *)buf, sizeof(buf));
+   if (code == B_INTERRUPTED) {
+       printf("[thread] receive_data interrupted\n");
+   }
+   else {
+       printf("[thread] got message %d: %s\n", code, buf);
+   }
 
-   snooze(1000000);
+   snooze(100000);
+   printf("[thread] snoozed 100000 - suspending\n");
    suspend_thread(find_thread(NULL));
+   printf("[thread] returning %d\n", code);
    return code;
 }
 
@@ -19,13 +31,18 @@ int main(int argc, char **argv) {
     int32 code = 63;
     const char *buf = "Hello";
 
+    printf("[main] %d:%d ready to spawn thread\n", getpid(), syscall(SYS_gettid));
     other_thread = spawn_thread(receiver, "receiver", 0, NULL);
+    printf("[main] spawned thread %d\n", other_thread);
     send_data(other_thread, code, (void *)buf, strlen(buf));
+    printf("[main] sent data - resuming thread %d\n", other_thread);
     resume_thread(other_thread);
 
-    snooze(100000);
+    snooze(1000000);
+    printf("[main] snoozed 1000000 - waiting for thread\n");
     status_t ret;
     wait_for_thread(other_thread, &ret);
+    printf("[main] got thread exit code %d\n", ret);
 
     return ret;
 }
