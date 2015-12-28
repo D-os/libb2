0de79f1ecc82d9af90677f450e3c58b337aaa3a5
Tomasz Sterna <tomek@xiaoka.com>
2015-12-29 00:27:24 +0100

    Fixed semaphore test

--- sem.cpp
+++ sem.cpp
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <syscall.h>
+#include <assert.h>
 
 #define atomic_inc(P) __sync_add_and_fetch((P), 1)
 #define atomic_dec(P) __sync_add_and_fetch((P), -1)
@@ -23,8 +24,10 @@ int32 worker(void *data)
             printf("[%s] error acquire_sem: %d\n", t.name, ret);
             return EXIT_FAILURE;
         }
+        //printf("[%s] yum!\n", t.name);
         atomic_dec(&number);
     }
+    printf("[%s] done!\n", t.name);
     return EXIT_SUCCESS;
 }
 
@@ -34,36 +37,57 @@ int main(int argc, char **argv) {
     number = 0;
 
     printf("[main] %d:%d creating semaphore\n", getpid(), (pid_t)syscall(SYS_gettid));
-    if ((sem = create_sem(1, "write")) < B_NO_ERROR) {
+    if ((sem = create_sem(number, "number")) < B_NO_ERROR) {
         printf("[main] failed creating semaphore: %ld\n", (ulong)sem);
         return EXIT_FAILURE;
     }
 
-    printf("[main] %d:%d spawning 999 workers\n", getpid(), (pid_t)syscall(SYS_gettid));
-    for (int i = 1; i <= 999; i++) {
-        sprintf(buf, "worker%d", i);
+    printf("[main] %d:%d spawning 99 workers\n", getpid(), (pid_t)syscall(SYS_gettid));
+    for (int i = 1; i <= 99; i++) {
+        sprintf(buf, "worker%02d", i);
         thread_id thread = spawn_thread(worker, buf, 0, NULL);
+        if (thread < 0) {
+            printf("[main] error spawning thread %d: %d\n", i, thread);
+            return EXIT_FAILURE;
+        }
         resume_thread(thread);
     }
 
+    /* 99 workers * each eats 10 = 990
+     * 90 nudges * each putting 11 = 990
+     */
+
+    snooze(100000);
     printf("[main] nudgeing 90x\n");
     for (int i=0; i < 90; i++) {
-        int tmp = number;
-        snooze(100000);
-        number = tmp + 1;
-        release_sem(sem);
-        tmp = number;
-        snooze(100000);
-        number = tmp + 10;
-        release_sem_etc(sem, 10, 0);
-        tmp = number;
-        snooze(100000);
-        number = tmp + 100;
-        release_sem_etc(sem, 100, 0);
+        snooze(10000);
+        number++;
+        status_t ret = release_sem(sem);
+        if (ret != B_NO_ERROR) {
+            printf("[main] error release_sem: %d\n", ret);
+            return EXIT_FAILURE;
+        }
+        snooze(10000);
+        number += 10;
+        ret = release_sem_etc(sem, 10, 0);
+        if (ret != B_NO_ERROR) {
+            printf("[main] error release_sem_etc: %d\n", ret);
+            return EXIT_FAILURE;
+        }
     }
-    snooze(1000000);
+    snooze(100000);
     printf("[main] done - number is %d\n", number);
-    // dump all running threads
+    assert(number == 0);
+
+    thread_info info;
+    int32 cookie = 0;
+
+    while (get_next_thread_info(0, &cookie, &info) == B_OK) {
+        printf("still running thread: %s\n", info.name);
+        if (info.state == B_THREAD_WAITING) {
+            printf("  waiting on semaphore %lu\n", info.sem);
+        }
+    }
 
     return EXIT_SUCCESS;
 }
