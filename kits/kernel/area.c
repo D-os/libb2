b2a7433a86ba3cc22006b471e7209c74ae150bc5
Tomasz Sterna <tomek@xiaoka.com>
2016-08-17 23:48:40 +0200

    Finished area implementation using shm

--- kits/kernel/area.c
+++ kits/kernel/area.c
@@ -7,26 +7,53 @@
 #include <sys/syscall.h>
 #include <sys/mman.h>
 #include <linux/mman.h>
+#include <string.h>
+#include <stdlib.h>
 
-area_id create_area(const char *name, void **startAddress, uint32 addressSpec,
-                    size_t size, uint32 lock, uint32 protection)
+#include "private.h"
+#include "utlist.h"
+#include "rwlock.h"
+
+typedef struct _area_info_struct {
+    int         shmid;
+    char        name[B_OS_NAME_LENGTH];
+    uint32		lock;
+    uint32      protection;
+    void        *address;
+    struct _area_info_struct *next;
+    struct _area_info_struct *prev;
+} _area_info;
+
+static _area_info *_areas = NULL;
+RWLOCK(_areas)
+
+/* WARNING! you need to lock _areas in caller function! */
+static _area_info *_find_area_info(area_id id)
 {
-    int shmid = shmget(IPC_PRIVATE, size, IPC_CREAT | 0600);
-    if (shmid < 0) {
-        switch (errno) {
-        case EINVAL:
-            return B_BAD_VALUE;
-        case ENFILE:
-        case ENOMEM:
-        case ENOSPC:
-            return B_NO_MEMORY;
-        default:
-            return B_ERROR;
-        }
-    }
+    _area_info *info;
+    int shmid = (int)id;
+    DL_SEARCH_SCALAR(_areas, info, shmid, shmid);
+    if (info) return info;
 
+    void *address = (void *)id;
+    DL_SEARCH_SCALAR(_areas, info, address, address);
+    return info;
+}
+static _area_info *_find_area_info_name(const char *name)
+{
+    _area_info *info;
+    DL_SEARCH_SCALAR(_areas, info, name, name);
+    return info;
+}
+
+static area_id _attach_shm_area(int shmid, const char *name,
+                                void **startAddress, uint32 addressSpec,
+                                size_t size, uint32 lock, uint32 protection)
+{
     void *shmaddr = NULL;
     int shmflg = 0;
+
+    if (!startAddress) return B_BAD_VALUE;
 
     switch (addressSpec) {
     case B_BASE_ADDRESS:
@@ -91,8 +118,167 @@ area_id create_area(const char *name, void **startAddress, uint32 addressSpec,
         }
     }
 
-    //FIXME: track all areas by name, id, address, size so other functions can work
+    _area_info *info = calloc(1, sizeof(_area_info));
+    info->shmid = shmid;
+    COPY_OS_NAME_LENGTH(info->name, name);
+    info->address = shmaddr;
+    info->lock = lock;
+    info->protection = protection;
+    _areas_wlock();
+    DL_APPEND(_areas, info);
+    _areas_unlock();
 
     *startAddress = shmaddr;
     return shmid;
+}
+
+area_id create_area(const char *name, void **startAddress, uint32 addressSpec,
+                    size_t size, uint32 lock, uint32 protection)
+{
+    int shmid = shmget(IPC_PRIVATE, size, IPC_CREAT | 0600);
+    if (shmid < 0) {
+        switch (errno) {
+        case EINVAL:
+            return B_BAD_VALUE;
+        case ENFILE:
+        case ENOMEM:
+        case ENOSPC:
+            return B_NO_MEMORY;
+        default:
+            return B_ERROR;
+        }
+    }
+    return _attach_shm_area(shmid, name, startAddress, addressSpec, size, lock, protection);
+}
+
+status_t delete_area(area_id id)
+{
+    _areas_rlock();
+    _area_info *info = _find_area_info(id);
+    _areas_unlock();
+    if(info) {
+        _areas_wlock();
+        DL_DELETE(_areas, info);
+        _areas_unlock();
+        free(info);
+    }
+    if (shmctl(id, IPC_RMID, NULL) != 0) {
+        switch (errno) {
+        case EIDRM:
+        case EINVAL:
+            return B_ERROR;
+        case EPERM:
+            return B_PERMISSION_DENIED;
+        default:
+            return B_ERROR;
+        }
+    }
+    return B_OK;
+}
+
+status_t set_area_protection(area_id id, uint32 newProtection)
+{
+    int shmid = -1;
+    void *shmaddr = NULL;
+    _areas_wlock();
+    _area_info *info = _find_area_info(id);
+    if (info) {
+        info->protection = newProtection;
+        shmaddr = info->address;
+        shmid = info->shmid;
+    }
+    _areas_unlock();
+    if (!info || !shmaddr) return B_BAD_VALUE;
+
+    int shmflg = SHM_REMAP;
+    if (!(newProtection & B_WRITE_AREA)) shmflg |= SHM_RDONLY;
+    shmaddr = shmat(shmid, shmaddr, shmflg);
+    if (shmaddr == (void *) -1) {
+        switch (errno) {
+        case EIDRM:
+        case EINVAL:
+            return B_BAD_VALUE;
+        case ENOMEM:
+            return B_NO_MEMORY;
+        default:
+            return B_ERROR;
+        }
+    }
+    return B_OK;
+}
+
+area_id find_area(const char *name)
+{
+    area_id id = -1;
+    _areas_rlock();
+    _area_info *info = _find_area_info_name(name);
+    if (info) id = info->shmid;
+    _areas_unlock();
+    return info ? id : B_NAME_NOT_FOUND;
+}
+
+area_id clone_area(const char *name, void **destAddress,
+                   uint32 addressSpec, uint32 protection, area_id source)
+{
+    int shmid = -1;
+    _areas_rlock();
+    _area_info *info = _find_area_info(source);
+    if (info) shmid = info->shmid;
+    _areas_unlock();
+    if (!info) shmid = (int)source;
+    struct shmid_ds ds = {};
+    if (shmctl(shmid, IPC_STAT, &ds) != 0) {
+        switch (errno) {
+        case EIDRM:
+        case EINVAL:
+            return B_BAD_VALUE;
+        case EPERM:
+            return B_PERMISSION_DENIED;
+        default:
+            return B_ERROR;
+        }
+    }
+    area_id clone = _attach_shm_area(shmid, name, destAddress, addressSpec, ds.shm_segsz, B_NO_LOCK, protection);
+    return info ? (intptr_t)*destAddress : clone;
+}
+
+status_t _get_area_info(area_id id, area_info *areaInfo, size_t size)
+{
+    _area_info *info = NULL;
+    int shmid = -1;
+
+    memset(areaInfo, 0, size);
+    areaInfo->area = id;
+
+    _areas_rlock();
+    info = _find_area_info(id);
+    if (info) {
+        shmid = info->shmid;
+        COPY_OS_NAME_LENGTH(areaInfo->name, info->name);
+        areaInfo->lock = info->lock;
+        areaInfo->protection = info->protection;
+        areaInfo->address = info->address;
+    }
+    _areas_unlock();
+    if (!info) return B_ERROR;
+
+    struct shmid_ds ds = {};
+    if (shmctl(shmid, IPC_STAT, &ds) != 0) {
+        goto error;
+    }
+    areaInfo->size = ds.shm_segsz;
+    areaInfo->team = ds.shm_cpid;
+    areaInfo->copy_count = ds.shm_nattch;
+
+    return B_OK;
+error:
+    switch (errno) {
+    case EIDRM:
+    case EINVAL:
+        return B_ERROR;
+    case EPERM:
+        return B_PERMISSION_DENIED;
+    default:
+        return B_ERROR;
+    }
 }
--- tests/kernel/area.cpp
+++ tests/kernel/area.cpp
@@ -4,13 +4,38 @@
 int main(int argc, char **argv) {
     setbuf(stdout, NULL); // do not buffer
 
-    void *addr = (void *)(intptr_t)(B_PAGE_SIZE * 100);
-    size_t size = B_PAGE_SIZE * 100;
+    void *addr = (void *)(intptr_t)(B_PAGE_SIZE * 0x80);
+    size_t size = B_PAGE_SIZE * 0xf;
     printf("before create_area addr: %p\n", addr);
     area_id id = create_area("test", &addr, B_BASE_ADDRESS, size, B_NO_LOCK, B_READ_AREA | B_WRITE_AREA);
-    printf(" after create_area addr: %p\tarea_id: %d\n", addr, id);
+    printf(" after create_area addr: %p\tarea_id: %zx\n", addr, id);
     char *first = (char*)addr, *last = first + size - 1;
     printf("> first: %d\tlast: %d\n", *first, *last);
     *first = *last = 0xff;
     printf("< first: %d\tlast: %d\n", *first, *last);
+
+    area_info areaInfo;
+    status_t status = get_area_info(id, &areaInfo);
+    if (status != B_OK) {
+        printf("error get_area_info(%zx): %d\n", id, status);
+        exit(EXIT_FAILURE);
+    }
+    printf("area %zx[%.*s] 0x%zx@%p x%d\n", areaInfo.area, B_OS_NAME_LENGTH, areaInfo.name, areaInfo.size, areaInfo.address, areaInfo.copy_count);
+
+    area_id id2 = clone_area("clone", &addr, B_ANY_ADDRESS, B_READ_AREA, id);
+    if (id2 < 0) {
+        printf("error clone_area(%zx): %zd\n", id, id2);
+        exit(EXIT_FAILURE);
+    }
+    printf("after clone_area addr: %p\tarea_id: %zx\n", addr, id2);
+
+    status = get_area_info(id2, &areaInfo);
+    if (status != B_OK) {
+        printf("error get_area_info(%zx): %d\n", id2, status);
+        exit(EXIT_FAILURE);
+    }
+    printf("area %zx[%.*s] 0x%zx@%p x%d\n", areaInfo.area, B_OS_NAME_LENGTH, areaInfo.name, areaInfo.size, areaInfo.address, areaInfo.copy_count);
+    printf("< first: %d\tlast: %d\n", *(char*)areaInfo.address, *((char*)areaInfo.address + areaInfo.size - 1));
+
+    delete_area(id);
 }
