009ae733d7380af96641c0251bca1447804fa8f5
Tomasz Sterna <tomasz@sterna.link>
2022-09-03 14:28:53 +0200

    !!! temporary remove all headers

--- kits/interface/Size.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright 2006, Ingo Weinhold <bonefish@cs.tu-berlin.de>.
- * All rights reserved. Distributed under the terms of the MIT License.
- */
-
-#include <Size.h>
-
-// completely implemented inline
