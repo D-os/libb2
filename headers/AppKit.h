80cb09c6180e80d18e76d1a1a643ae1056583562
Tomasz Sterna <tomasz@sterna.link>
2022-10-11 10:08:39 +0200

    Add kits headers

---
+++ headers/AppKit.h
@@ -0,0 +1,13 @@
+// #include <AppDefs.h>
+#include <Application.h>
+// #include <Clipboard.h>
+#include <Handler.h>
+// #include <Invoker.h>
+#include <Looper.h>
+#include <Message.h>
+// #include <MessageFilter.h>
+// #include <MessageQueue.h>
+// #include <MessageRunner.h>
+// #include <Messenger.h>
+// #include <PropertyInfo.h>
+#include <Roster.h>
---
+++ headers/KernelKit.h
@@ -0,0 +1,9 @@
+#include <OS.h>
+// #include <debugger.h>
+// #include <fs_attr.h>
+// #include <fs_index.h>
+// #include <fs_info.h>
+// #include <fs_query.h>
+#include <image.h>
+// #include <perfmon_user.h>
+// #include <scheduler.h>
